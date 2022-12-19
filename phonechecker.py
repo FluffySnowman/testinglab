7da659851e6a41d36f7007820cdd000172f5ee13
sh44nk3v1n <51316255+FluffySnowman@users.noreply.github.com>
2022-12-19 05:39:55 +0530

    added carrier and timezone/location for phonechecker.py

--- phonechecker.py
+++ phonechecker.py
@@ -1,12 +1,19 @@
 import phonenumbers
+from phonenumbers import timezone
+from phonenumbers import carrier
 
-phone_number = phonenumbers.parse("+1-234-567-8901", "US")
+phone_number = phonenumbers.parse("+441234567890")
 
-print("Country code:", phone_number.country_code)
+""" print("Country code:", phone_number.country_code)
 print("National number:", phone_number.national_number)
 print("E164 format:", phonenumbers.format_number(phone_number, phonenumbers.PhoneNumberFormat.E164))
-
 if phonenumbers.is_valid_number(phone_number):
     print("The phone number exists yay")
 else:
     print("The phone number doesn't exist you dumb fuck")
+
+"""
+
+print(timezone.time_zones_for_number(phone_number))
+print(carrier.name_for_number(phone_number, "en"))
+
